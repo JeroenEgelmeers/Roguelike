73aeebcbcc54799f40ca73c15e39f0032e63004f
Jeroen Egelmeers <jeroenegelmeers@gmail.com>
2015-09-29 13:12:52 +0200

    Tried few things like entering username etc.

--- Dungeons/Dungeons/Dungeons.cpp
+++ Dungeons/Dungeons/Dungeons.cpp
@@ -2,10 +2,29 @@
 //
 
 #include "stdafx.h"
-
+#include <string>
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	string currentAction;
+
+	// Begin beschrijving
+	cout << "Welkom bij Roguelike!" << endl;
+	cout << "Laten we beginnen! Wat is je naam?" << endl;
+	string userName;
+	cin >> userName;
+	cout << "Welkom " << userName << ". Je bent nu klaar om te beginnen. Als je zover bent, typ dan: !enterDungeon" << endl;
+	cin.clear();
+	
+	cin >> currentAction;
+	if (currentAction == "!enterDungeon") {
+		cout << "Je komt de eerste ruimte binnen." << endl;
+		cout << "Beschrijving: {eerste kamer}" << endl;
+	}
+	
+	while (true) {
+		// Keep it alive!
+	}
 	return 0;
 }
-
